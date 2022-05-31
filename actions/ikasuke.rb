9facf95c4b26cddc07a3f9c22c4a14dc61358776
Yusuke Nakamura <yusuke1994525@gmail.com>
2022-05-31 22:54:45 +0900

    Use yaml instead of class const

--- actions/ikasuke.rb
+++ actions/ikasuke.rb
@@ -1,107 +1,45 @@
 require 'json'
 require 'time'
+require 'yaml'
+
 module Ruboty
   module Actions
     class Ikasuke < Base
-      BRAND_LIST =
-        {
-          "アイロニック" => {good: "スーパージャンプ時間短縮", bad: "復活時間短縮"},
-          "アナアキ" => {good: "メイン性能アップ", bad: "スペシャル減少ダウン"},
-          "アロメ" => {good: "インク回復力アップ", bad: "スーパージャンプ時間短縮"},
-          "エゾッコ" => {good: "スペシャル減少量ダウン", bad: "スペシャル増加量アップ"},
-          "エンペリー" => {good: "サブ性能アップ", bad: "相手インク影響軽減"},
-          "クラーゲス" => {good: "イカダッシュ速度アップ", bad: "爆風ダメージ軽減・改"},
-          "シグレニ" => {good: "爆風ダメージ軽減・改", bad: "メイン性能アップ"},
-          "ジモン" => {good: "インク効率アップ（メイン）", bad: "ヒト移動速度アップ"},
-          "タタキケンサキ" => {good: "メイン性能アップ", bad: "サブ性能アップ"},
-          "バトロイカ" => {good: "相手インク影響軽減", bad: "インク効率アップ（メイン）"},
-          "フォーリマ" => {good: "スペシャル性能アップ", bad: "インク効率アップ（サブ）"},
-          "ホタックス" => {good: "復活時間短縮", bad: "スペシャル減少量ダウン"},
-          "ホッコリー" => {good: "インク効率アップ（サブ）", bad: "インク回復力アップ"},
-          "ロッケンベルグ" => {good: "ヒト移動速度アップ", bad: "イカダッシュ速度アップ"},
-          "ヤコ" => {good: "スペシャル増加量アップ", bad: "スペシャル性能アップ"},
-          "アタリメイド" => {good: "なし", bad: "なし"},
-          "アミーボ" => {good: "なし", bad: "なし"},
-          "クマサン商会" => {good: "なし", bad: "なし"},
-        }
-
-      GPOWER_LIST =
-        {
-          "相手インク影響軽減" => "バトロイカ",
-          "安全靴" => "バトロイカ",
-          "インク効率アップ(メイン)" => "ジモン",
-          "メインク" => "ジモン",
-          "ヒト移動速度アップ" => "ロッケンベルグ",
-          "人速" => "ロッケンベルグ",
-          "ヒト速" => "ロッケンベルグ",
-          "イカダッシュ速度アップ" => "クラーゲス",
-          "イカ速" => "クラーゲス",
-          "爆風ダメージ軽減・改" => "シグレニ",
-          "爆風ダメ軽減" => "シグレニ",
-          "爆風軽減" => "シグレニ",
-          "爆風" => "シグレニ",
-          "マキガ" => "シグレニ",
-          "スーパージャンプ時間短縮" => "アイロニック",
-          "スパジャン短縮" => "アイロニック",
-          "スパジャン" => "アイロニック",
-          "復活時間短縮" => "ホタックス",
-          "復活時間" => "ホタックス",
-          "復活" => "ホタックス",
-          "ゾンビ" => "ホタックス",
-          "スペシャル減少量ダウン" => "エゾッコ",
-          "スペ減" => "エゾッコ",
-          "スペシャル増加量" => "ヤコ",
-          "スペ増" => "ヤコ",
-          "スペシャル性能アップ" => "フォーリマ",
-          "スペ強" => "フォーリマ",
-          "スペ性" => "フォーリマ",
-          "インク効率アップ(サブ)" => "ホッコリー",
-          "サブインク" => "ホッコリー",
-          "サインク" => "ホッコリー",
-          "インク回復力アップ" => "アロメ",
-          "インク回復力" => "アロメ",
-          "インク回復" => "アロメ",
-          "サブ性能アップ" => "エンペリー",
-          "サブ性能" => "エンペリー",
-          "サブ強化" => "エンペリー",
-          "メイン性能アップ" => "アナアキ or タタキケンサキ",
-          "メイン性能" => "アナアキ or タタキケンサキ",
-          "メ性" => "アナアキ or タタキケンサキ",
-      }
+      @@data = YAML.load_file("data.yaml")["splatoon"]
 
       def self.brand_list(message)
         body = ''
-        BRAND_LIST.each do |key, value|
+        @@data["brands"].each do |brand|
           body << "--------------------------\n"
-          body << "#{key}=> 付きやすい : #{value[:good]} 付きにくい : #{value[:bad]}\n"
+          body << "#{brand["name"]} => 付きやすい : #{brand["good"]} 付きにくい : #{brand["bad"]}\n"
         end
         message.reply body
       end
 
       def self.brand(message)
-        begin
-          message.reply "付きやすいギア : " + BRAND_LIST[message[:brand_name]][:good]
-          message.reply "付きにくいギア : " + BRAND_LIST[message[:brand_name]][:bad]
-        rescue NoMethodError
+        brand = @@data["brands"].find { |brand| brand["name"] == message[:brand_name] }
+        if brand
+          message.reply "付きやすいギア : " + brand["good"]
+          message.reply "付きにくいギア : " + brand["bad"]
+        else
           message.reply "ブランドがみつかりません"
         end
       end
 
       def self.gearpower_list(message)
         body = ''
-        GPOWER_LIST.each do |key, value|
-          body << "#{key} が付きやすいブランドは #{value}\n\n"
+        @@data["gear_powers"].each do |gear_power|
+          body << "#{gear_power["name"]} が付きやすいブランドは #{gear_power["target"]}\n\n"
         end
         message.reply body
       end
 
       def self.gearpower(message)
-        begin
-          message.reply "付きやすいブランド : " + GPOWER_LIST[message[:gpower_name]]
-        rescue NoMethodError
+        gear = @@data["gear_powers"].find { |gear| gear["name"] == message[:gpower_name] || gear["aliases"].include?(message[:gpower_name]) }
+        if gear
+          message.reply "付きやすいブランド : " + gear["target"]
+        else
           message.reply "Not Found"
-        rescue TypeError => ex
-          message.reply "TypeError"
         end
       end
 
@@ -172,7 +110,6 @@ module Ruboty
         body << "ブキ : " + weapon_text
         message.reply body
       end
-
     end
   end
 end
