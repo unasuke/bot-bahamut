14180c88bfa5d91c20f4fa385b577abd99ea0fca
JtRaiDen1014 <rainy.incarnation@gmail.com>
2026-04-04 22:45:01 +0900

    Create db.rb

---
+++ lib/db.rb
@@ -0,0 +1,15 @@
+require 'sqlite3'
+
+DB = SQLite3::Database.new 'reminders.db'
+
+DB.execute <<-SQL
+CREATE TABLE IF NOT EXISTS reminders (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id TEXT,
+  channel_id TEXT,
+  text TEXT,
+  remind_at INTEGER,
+  status TEXT,
+  completed_at INTEGER
+)
+SQL
