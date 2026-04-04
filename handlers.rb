24380833cea41554d02d4ca87cf933fde620786f
JtRaiDen1014 <rainy.incarnation@gmail.com>
2026-04-04 22:49:53 +0900

    Update handlers.rb

--- handlers.rb
+++ handlers.rb
@@ -1,3 +1,5 @@
 require 'ruboty/discord' unless ENV['LOCAL']
 require './handlers/ai_reply'
 require './actions/ai_reply'
+require_relative 'handlers/remind'
+require_relative 'handlers/remind_checker'
